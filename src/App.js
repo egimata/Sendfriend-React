61c724dd2f0cc8c5d60ec37aede04be88358f32c
Marlen0307 <marlen.hima@gmail.com>
2021-12-11 22:02:27 +0100

    addinghistory component

--- src/App.js
+++ src/App.js
@@ -2,14 +2,14 @@ import logo from './logo.svg';
 import './App.css';
 import { Homepage } from './components/Homepage';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
-import {Contact} from './components/Contact';
+import {History} from './components/History';
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path = '/' element = {<Homepage />} / >
-        <Route path = '/contact' element = {<Contact />} / >
+        <Route path = '/history' element = {<History />} / >
       </Routes>
     </Router>
   );
