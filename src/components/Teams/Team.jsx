a7373831424ccd03eeabb982caa5fdb0b0bc121f
Marlen Hima <m.hima@evolutivo.it>
2022-05-06 11:46:32 +0200

    opacity fix

--- src/components/Teams/Team.jsx
+++ src/components/Teams/Team.jsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import {Link} from "react-router-dom";
 import { HashLink } from 'react-router-hash-link';
 import ScrollAnimation from "react-animate-on-scroll";
+import { TeamMemer } from './TeamMemer';
 
 
 const teamData = [
@@ -27,11 +28,7 @@ const teamData = [
 
 
 const TeamOne = ({column , teamStyle}) => {
-    const [opacity, setOpacity] = useState(0);
-
-    const changeOpacity = (opac) => {
-        setOpacity(opac);
-    }
+  
     return (<>
 <div className="third-section bg-dark-two">
 
@@ -104,34 +101,7 @@ const TeamOne = ({column , teamStyle}) => {
         <div className="third-section bg-dark-two">
            <div className={`row ${column}`}>
                {teamData.map((data, index) => (
-                   <div className={data.column} key={index} onMouseEnter = {()=>changeOpacity(1)} onMouseLeave = {()=>changeOpacity(0)}>
-                       <div className={`rn-team ${teamStyle}`}>
-                           <ScrollAnimation 
-                               animateIn="fadeInUp"
-                               animateOut="fadeInOut"
-                               animateOnce={true}
-                               delay={1000}>
-                                   
-                               <div className="inner">
-                                    <HashLink smooth to={data.link}>
-                                        <figure className="thumbnail">
-                                            <img src={`./img/team/${data.image}.jpg`} alt={data.name}/>
-                                        </figure>
-                                   </HashLink>
-                                   <figcaption className="content">
-                                  
-                                       <div class="bio-x0uKuI" style={{opacity: opacity}} >
-                                           <div class="rectangle-45-rs3s2b"></div>
-                                           <p className="title">{data.name}</p>
-                                           <p className="subtitle theme-gradient">{data.designation}</p>
-                                       </div>
-     
-                                       
-                                   </figcaption>
-                               </div>
-                           </ScrollAnimation>
-                       </div>
-                   </div>
+                  <TeamMemer key={index} memmberInfo={data} teamStyle = {teamStyle}  / >
                ))}
            </div>
        </div>
